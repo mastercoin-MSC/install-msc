a5fc76497ae0a801a78522748c0a4dc07833f0ed
achamely <achamely@users.noreply.github.com>
2014-03-13 11:09:17 -0500

    added help/root check
    Modified from sx install script added check for root and runtime menu.
    Added ability to specify obelisk server details on cli

--- install-msc.sh
+++ install-msc.sh
@@ -2,9 +2,49 @@
 #Instructions are for Ubuntu 13.04 and newer
 
 #get the current directory
-SRC=`pwd`
 
-echo $SRC
+set -e
+echo
+echo " [+] Welcome to S(pesmilo)X(changer)."
+echo
+if [ "$#" = "2" ]; then
+    if [[ "$1" = "-os" ]]; then
+        #Absolute path
+        SERVER=$2
+        PREFIG=CLE
+    else 
+    	HELP=1
+    fi
+fi
+	
+if[ "$1" = "--help" ] || [ $HELP ]; then
+     echo " [+] Install script help:"
+     echo " --> To execute this script type:"
+     echo " <sudo bash install-msc.sh>"
+     echo " --> To execute this script and install with a specific obelisk server"
+     echo " <bash install-msc.sh -os server-details:port>"
+     echo " This script will install SX and the required prerequisites"
+     echo " The SX install script will install libbitcoin, libwallet, obelisk and sx tools."
+     echo " The standard path for the installation is /usr/local/"
+     echo " The stardard path for the conf files is /etc."
+     echo
+     exit
+else
+
+if [ `id -u` = "0" ]; then
+    SRC=`pwd`
+else
+    echo
+    echo "[+] ERROR: This script must be run as root." 1>&2
+    echo
+    echo "<sudo bash install-msc.sh>"
+    echo
+    exit
+fi
+
+
+
+
 
 while [ -z "$PREFIG" ]; do 
 	echo "Do you have an obelisk server and wish to enter its details now? [y/n]"
@@ -13,22 +53,28 @@ done
 
 case $PREFIG in
 	y* | Y* )
-		active=1
+		ACTIVE=1
 		CONFIRM=no	
-	;;	
+	;;
+	
+	CLE)
+		ACTIVE=1
+		CONFIRM=P
+	;;
+	
 	*)
 		active=0
 	;;
 esac
 
-while [ $active -ne 0 ]; do
+while [ $ACTIVE -ne 0 ]; do
 	case $CONFIRM in
 
 	y* | Y* )
 		echo "Writing Details to ~/.sx.cfg"
 		echo "You can update/change this file as needed later"
 		echo "service = \""$SERVER"\"" > ~/.sx.cfg
-		active=0
+		ACTIVE=0
 	;;
 	
 	n* | N* )
@@ -38,10 +84,13 @@ while [ $active -ne 0 ]; do
 			echo "If you don't have one yet enter anything, you can update/change this later"
 			read SERVER
 		done
-
+		CONFIRM=P
+	;;
+	
+	P)
 		echo "You entered: "$SERVER
 		echo "Is this correct? [y/n]"
-		read CONFIRM			
+		read CONFIRM
 	;;
 	
 	*)
