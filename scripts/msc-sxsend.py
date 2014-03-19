7d4b7e9dbdb819af85d4fd27c9f98c9c2981336f
achamely <shmali81@gmail.com>
2014-03-18 20:30:08 -0500

    removed testing balances

--- scripts/msc-sxsend.py
+++ scripts/msc-sxsend.py
@@ -48,12 +48,8 @@ BAL = commands.getoutput('sx balance -j '+listOptions['transaction_from'])
 balOptions = json.loads(str(''.join(BAL)))
 available_balance = int(balOptions[0]['paid'])
 
-#broadcast_fee = int(10000)
-#output_minimum = int(5555) #dust threshold
-
-broadcast_fee = int(1000)
-output_minimum = int(1500) #dust threshold
-
+broadcast_fee = int(10000)
+output_minimum = int(5555) #dust threshold
 
 fee_total = broadcast_fee + (output_minimum * 4)
 
