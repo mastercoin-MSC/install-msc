8f6aaf4f43bdfe7ddbb45e1cf73328c3b4370b04
Sean Gilligan <sean@msgilligan.com>
2014-05-18 11:43:18 +0200

    Add ‘curl’ to installed packages.

--- install-prerequisites.sh
+++ install-prerequisites.sh
@@ -4,5 +4,5 @@ sudo apt-get update
 #install packages:
 sudo apt-get -y install git python-simplejson python-git python-pip
 sudo apt-get -y install make
-sudo apt-get -y install git build-essential autoconf libtool libboost-all-dev pkg-config libcurl4-openssl-dev libleveldb-dev libzmq-dev libconfig++-dev libncurses5-dev
+sudo apt-get -y install git curl build-essential autoconf libtool libboost-all-dev pkg-config libcurl4-openssl-dev libleveldb-dev libzmq-dev libconfig++-dev libncurses5-dev
 sudo pip install -r pip.packages
