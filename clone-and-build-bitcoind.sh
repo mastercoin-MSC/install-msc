c35bd0cb5d838c3972a8213d5ee4a43540fa3375
Sean Gilligan <sean@msgilligan.com>
2014-05-29 11:44:36 -0700

    Add ‘-y’ & some more dependencies to apt-get

--- clone-and-build-bitcoind.sh
+++ clone-and-build-bitcoind.sh
@@ -5,12 +5,12 @@ REPODIR=$3
 
 echo Installing packages needed to build bitcoind/mastercoind...
 
-sudo apt-get install libssl-dev
-sudo apt-get install software-properties-common
+sudo apt-get -y install libssl-dev
+sudo apt-get -y install software-properties-common
 sudo add-apt-repository ppa:bitcoin/bitcoin
 sudo apt-get update
-sudo apt-get install libdb4.8-dev
-sudo apt-get install libdb4.8++-dev
+sudo apt-get -y install libdb4.8-dev libdb4.8++-dev
+sudo apt-get -y install libprotobuf-dev protobuf-compiler 
 
 # Should we install QT here or provide an option to do it?
 
