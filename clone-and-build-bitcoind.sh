82204fa0594a0b01f613ae76d9bbc040c94ecae2
Sean Gilligan <sean@msgilligan.com>
2014-06-12 14:19:07 -0700

    Move MasterCore pkg dependencies to base box

--- clone-and-build-bitcoind.sh
+++ clone-and-build-bitcoind.sh
@@ -3,15 +3,6 @@ REPOURL=$1
 BRANCH=$2
 REPODIR=$3
 
-echo Installing packages needed to build bitcoind/mastercoind...
-
-sudo apt-get -y install libssl-dev
-sudo apt-get -y install software-properties-common
-sudo add-apt-repository ppa:bitcoin/bitcoin
-sudo apt-get update
-sudo apt-get -y install libdb4.8-dev libdb4.8++-dev
-sudo apt-get -y install libprotobuf-dev protobuf-compiler 
-
 # Should we install QT here or provide an option to do it?
 
 git clone --no-checkout $REPOURL $REPODIR
